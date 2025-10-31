74df17bdc9b475550174ae3d52ca4a6a01780444
sagarbansal <bansalsagar13@gmail.com>
2025-10-31 23:52:29 +0530

    working on aws configuration

---
+++ src/main/java/com/tastenfood/FoodApp/aws/AWSS3Service.java
@@ -0,0 +1,13 @@
+package com.tastenfood.FoodApp.aws;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URL;
+
+public interface AWSS3Service {
+
+    URL uploadFile(String keyName, MultipartFile file);
+
+    void deleteFile(String keyName);
+
+}
